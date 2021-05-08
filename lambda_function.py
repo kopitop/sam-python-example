10bb552f8a6bf947d22801abb795ec96c45e85e6
Nguyen Minh Hiep (JODC.HN) <HiepNM3@fsoft.com.vn>
2021-05-08 17:45:15 +0700

    change name

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Project One for BKHN 2021')
+        'body': json.dumps('Project One for BKHN 2021. Successful response')
     }
